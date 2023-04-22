5bf6060d66c107eb4c66d93134db75281a7bd319
JNeto <joaomendes15@gmail.com>
2023-04-22 18:30:22 +0100

    LP1 semana 06 added

---
+++ Semester 2/Linguagens de Programacao 1/LP1Semana06/GameSixFriday/GameLevel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameSixFriday
+{
+    public class GameLevel
+    {
+        private struct Room
+        {
+            public int adress;
+            public List<Foe> foes;
+            public Room(int adress) : this()
+            {
+                this.adress = adress;
+                foes = new List<Foe>();
+            }
+            public override string ToString() => $"Room {adress}: {string.Join(',', foes)}";
+        }
+
+        private int totalRooms;
+        private Difficulty difficulty;
+        private static int totFoes;
+        private Room[] rooms;
+        
+        public GameLevel(int totalRooms, Difficulty difficulty)
+        {
+            this.totalRooms = totalRooms;
+            this.difficulty = difficulty;
+            this.rooms = new Room[totalRooms];
+            for (int i = 0; i < totalRooms; i++)
+                this.rooms[i] = new Room(i);
+        }
+
+        public void SetFoeInRoom(int roomAddress, Foe foe)
+        {
+            rooms[roomAddress].foes.Add(foe);
+            totFoes += 1;
+        }
+
+        public Difficulty GetDifficulty() => this.difficulty;
+        public int GetNumRooms() => this.totalRooms;
+        public int GetNumFoes() => totFoes;
+
+        public void PrintFoes()
+        {
+            for (int i = 0; i < totalRooms; i++)
+            {
+                if (rooms[i].foes.Count == 0)
+                    continue;
+                Console.WriteLine($"{rooms[i]}");
+            }
+        }
+    }
+}
